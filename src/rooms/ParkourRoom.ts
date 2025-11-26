7e2022f0952bcf0e25e9d3df5bb028016d214a5e
m.@G <maneegoon@gmail.com>
2025-11-26 13:49:39 +0530

    Update ParkourRoom.ts

--- src/rooms/ParkourRoom.ts
+++ src/rooms/ParkourRoom.ts
@@ -291,7 +291,7 @@ export class ParkourRoom extends Room<ParkourRoomState> {
   private generateRoomId(): string {
     const chars = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
     let code = "";
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 4; i++) {
       code += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return code;
